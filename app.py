2de6b9ee6fc5c951f65ce5d826c366cea869d8ba
Allie c( ⁰ 〰 ⁰ )੭ <88z@pm.me>
2018-05-26 21:17:41 -0700

    include eth as base currency

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import json
 import locale
 import sys
@@ -6,11 +7,12 @@ import urllib2
 locale.setlocale(locale.LC_ALL, locale.getdefaultlocale())
 
 
-CURRENCY_SYMBOL = locale.localeconv()['int_curr_symbol'].lower().strip() or 'usd'
-
+#CURRENCY_SYMBOL = locale.localeconv()['int_curr_symbol'].lower().strip() or 'usd'
+#override with eth
+CURRENCY_SYMBOL = 'eth'
 
 def make_response():
-    url = 'https://api.coinmarketcap.com/v1/ticker/?convert={}'.format(CURRENCY_SYMBOL)
+    url = 'https://api.coinmarketcap.com/v1/ticker/?limit=0&convert={}'.format(CURRENCY_SYMBOL)
     try:
         response = urllib2.urlopen(url).read()
         data = json.loads(response)
@@ -20,14 +22,22 @@ def make_response():
 
 
 def make_item(coin):
-    price_key = 'price_{}'.format(CURRENCY_SYMBOL)
+    #price_key = 'price_{}'.format(CURRENCY_SYMBOL)
+    price_key = 'price_usd'
     market_cap_key = 'market_cap_{}'.format(CURRENCY_SYMBOL)
-    coin['price'] = locale.currency(float(coin[price_key]), grouping=True)
-    coin['market_cap'] = locale.currency(float(coin[market_cap_key]), grouping=True)
+    coin['price_eth'] = u"Ξ{}".format(coin['price_eth'])
+    if not coin[price_key]:
+        coin['price'] = 'null'
+    else:
+        coin['price'] = locale.currency(float(coin[price_key]), grouping=True)
+    if not coin[market_cap_key]:
+        coin['market_cap'] = 'null'
+    else:
+        coin['market_cap'] = locale.currency(float(coin[market_cap_key]), grouping=True)
     return {
         'uid': coin['rank'],
-        'title': '{} - {} - {}'.format(coin['name'], coin['symbol'], coin['price']),
-        'subtitle': 'Market cap: {}'.format(coin['market_cap']),
+        'title': u"{} - {} - {} | {}".format(coin['name'], coin['symbol'], coin['price'], coin['price_eth']),
+        'subtitle': 'Marker cap: {}'.format(coin['market_cap']),
         'type': 'default',
         'icon': {
             'path': './icons/{}.png'.format(coin['symbol'].lower())
@@ -53,6 +63,8 @@ def output(data):
 
 data = make_response()
 
-query = sys.argv[1]
-
-sys.stdout.write(json.dumps(output(filter_data(data, query))))
+if len(sys.argv) == 1:
+    sys.stdout.write(json.dumps(output(list(data))))
+else:
+    query = sys.argv[1]
+    sys.stdout.write(json.dumps(output(filter_data(data, query))))
